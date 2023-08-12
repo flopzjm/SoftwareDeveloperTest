41326a6fd1bc6d2c8fa58fcf0335776731925ba4
flopez <flopez@patprimo.com.co>
2023-08-11 22:47:53 -0500

    Configuracion Incial

--- SoftwareDeveloper.Business/EmployeeBO.cs
+++ SoftwareDeveloper.Business/EmployeeBO.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SoftwarePresentation.DataAccess
+namespace SoftwareDeveloper.Business
 {
-    public class EmployeeDAO
+    public class EmployeeBO
     {
+
     }
 }
---
+++ SoftwareDeveloper.Entities/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace SoftwareDeveloper.Entities
+{
+
+
+    public class EmployeeDTO
+    {
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("data")]
+        public List<Datum> Data { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+    }
+
+    public class Datum
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("employee_name")]
+        public string EmployeeName { get; set; }
+
+        [JsonProperty("employee_salary")]
+        public long EmployeeSalary { get; set; }
+
+        [JsonProperty("employee_age")]
+        public long EmployeeAge { get; set; }
+
+        [JsonProperty("profile_image")]
+        public string ProfileImage { get; set; }
+    }
+
+
+}
