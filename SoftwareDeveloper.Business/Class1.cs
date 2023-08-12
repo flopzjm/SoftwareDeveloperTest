5cea9a1b5a5c5a8db2fafcca268f2f296b37e1cb
flopez <flopez@patprimo.com.co>
2023-08-11 22:23:36 -0500

    Add project files.

---
+++ SoftwareDeveloper.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SoftwareDeveloper.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ SoftwarePresentation.DataAccess/EmployeeDAO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SoftwarePresentation.DataAccess
+{
+    public class EmployeeDAO
+
+    {
+
+    }
+}
