ba6ab0cc26ad7755a5d563e5d7c349f3e754d3dd
flopez <flopez@patprimo.com.co>
2026-04-27 18:11:37 -0500

    Update EmployeeBO.cs
    se crea de la funcion calculo de salario anual

--- Business/EmployeeBO.cs
+++ Business/EmployeeBO.cs
@@ -5,5 +5,14 @@ namespace Business
     public class EmployeeBO
     {
         //make common business logic here
+        //haz una funcion recursiva para calcular el salario anual de un empleado dado su salario mensual
+        public decimal CalculateAnnualSalary(decimal monthlySalary, int monthsWorked)
+        {
+            if (monthsWorked <= 0)
+            {
+                return 0;
+            }
+            return monthlySalary + CalculateAnnualSalary(monthlySalary, monthsWorked - 1);
+        }
     }
 }
